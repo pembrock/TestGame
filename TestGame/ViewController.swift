2e1f85d467a76ce5e7c0a9af740e60ca8bf5ee00
pembrock <pembrock@gmail.com>
2019-07-10 15:01:23 +0300

    Initial Commit

---
+++ TestGame/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  TestGame
+//
+//  Created by Pasikuta Kirill on 08.07.2019.
+//  Copyright © 2019 Pasikuta Kirill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var gameFieldView: UIView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var shapeX: NSLayoutConstraint!
+    @IBOutlet weak var shapeY: NSLayoutConstraint!
+    
+    private var isGameActive = false
+    private var gameTimeLeft: TimeInterval = 0
+    private var gameTimer: Timer?
+    private var timer: Timer?
+    private var displayDuration: TimeInterval = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        gameFieldView.layer.borderWidth = 1
+        gameFieldView.layer.borderColor = UIColor.gray.cgColor
+        gameFieldView.layer.cornerRadius = 5
+    }
+
+    private func startGame() {
+        //gameFieldView.isHidden = false
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(
+            timeInterval: displayDuration,
+            target: self,
+            selector: #selector(moveImage),
+            userInfo: nil,
+            repeats: true
+        )
+        timer?.fire()
+        gameTimer?.invalidate()
+        gameTimer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(gameTimerTick),
+            userInfo: nil,
+            repeats: true
+        )
+        gameTimeLeft = stepper.value
+        isGameActive = true
+        updateUI()
+    }
+    private func stopGame() {
+        isGameActive = false
+        updateUI()
+        gameTimer?.invalidate()
+        timer?.invalidate()
+    }
+    
+    private func updateUI() {
+        stepper.isEnabled = !isGameActive
+        if isGameActive {
+            timeLabel.text = "Осталось: \(Int(gameTimeLeft)) сек"
+            actionButton.setTitle("Остановить", for: .normal)
+        } else {
+            timeLabel.text = "Время: \(Int(stepper.value)) сек"
+            actionButton.setTitle("Начать", for: .normal)
+        }
+    }
+    
+    @objc private func gameTimerTick() {
+        gameTimeLeft -= 1
+        if gameTimeLeft <= 0 {
+            stopGame()
+        } else {
+            updateUI()
+        }
+    }
+    
+    @objc private func moveImage() {
+        let maxX = 100;
+        let maxY = 100;
+        
+        shapeX.constant = CGFloat(arc4random_uniform(UInt32(maxX)))
+        shapeY.constant = CGFloat(arc4random_uniform(UInt32(maxY)))
+    }
+    
+    @IBAction func actionButtonTapped(_ sender: UIButton) {
+        if isGameActive {
+            stopGame()
+        } else {
+            startGame()
+        }
+        
+    }
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        updateUI()
+    }
+}
+
